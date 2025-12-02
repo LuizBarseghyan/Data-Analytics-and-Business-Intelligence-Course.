7ac283df553834d2a5973f01104a6b1b61ec7567
LuizBarseghyan <barseghyanluiza00@gmail.com>
2025-12-02 18:24:41 +0000

    rectangle update

---
+++ Python/rectangle_area.py
@@ -0,0 +1,10 @@
+lenght = float(input("Enter the length of rectangle:"))
+width = float(input("Enter the width of rectangle:"))
+area = lenght*width
+print("The area of rectangel is", area)
+
+
+if area < 100:
+ print ("The area is too small")
+else:
+ print("The area is greater than or equal to 100")
